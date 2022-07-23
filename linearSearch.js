3a64ac44324778e81c6bc7351031b4a34a6a7771
Raj Dutta <rd2249619@gmail.com>
2022-07-23 13:35:06 +0600

    add linear search algorithms

---
+++ linearSearch.js
@@ -0,0 +1,18 @@
+
+function linearSearch(arr, num) {
+    let ans = -1;
+
+    for ( let i = 0; i < arr.length; i++ ) {
+        if (arr[i] === num) {
+            ans = i;
+        }
+    }
+
+    return ans;
+}
+
+const array = [1, 6, 74, 4, 3, 2, 10, 58, 30];
+
+const result = linearSearch(array, 30);
+
+console.log('linear Search Algorithms ', result);
