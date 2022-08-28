4202447d176fc2811b18e26a9d6694745fd24c1e
Raj Dutta <rd2249619@gmail.com>
2022-08-28 20:13:17 +0600

    add reverseInt function

--- coding_problems/reverseString/index.js
+++ coding_problems/reverseString/index.js
@@ -12,3 +12,32 @@ function reverseString(str) {
 const value = reverseString('hello world!');
 
 console.log(value);
+
+function reverseInt(num) { // O(n)
+    const isPositive = num > 0 ? true : false;
+    let str;
+    
+    if (isPositive) {
+        str = num.toString();
+    } else {
+        str = num.toString().slice(1);
+    }
+
+    let main = '';
+    if (str.length === 1) {
+        return isPositive ? parseInt(str) : parseInt(`-${str}`);
+    } else {
+        for ( let i = str.length - 1; i >= 0; i-- ) {
+            main += str[i];
+        }
+        
+        if (!isPositive) {
+            main = '-' + main;
+        }
+
+        return parseInt(main); 
+    }
+}
+
+const s = reverseInt(256);
+console.log(s);
