4729e9555cbf18a11e22e3d93cfd0fdfd90d8959
Raj Dutta <rd2249619@gmail.com>
2022-09-01 23:30:36 +0600

    add arrayChunk

---
+++ coding_problems/arrayChunk/index.js
@@ -0,0 +1,15 @@
+function arrayChunk(array, size) {
+    const main = [];
+    const loopCount = Math.ceil(array.length / size);
+
+    for (let i = 0; i < loopCount; i++) {
+        const resu = array.splice(0, size);
+        if (resu.length !== 0) {
+            main.push(resu);
+        }
+    }
+    return main;
+}
+
+const v = arrayChunk([1, 2, 3, 4, 5], 2);
+console.log('value1', v);
---
+++ index.js
@@ -0,0 +1,44 @@
+/*
+function binarySearchRecursive (arr, x, low = 0, high = arr.length - 1) {
+    const mid = Math.floor((low + high) / 2);
+
+    if (high >= low) {
+      if (arr[mid] === x) {
+        // item found => return its index
+        return mid
+      }
+  
+      if (x < arr[mid]) {
+        // arr[mid] is an upper bound for x, so if x is in arr => low <= x < mid
+        return binarySearchRecursive(arr, x, low, mid - 1)
+      } else {
+        // arr[mid] is a lower bound for x, so if x is in arr => mid < x <= high
+        return binarySearchRecursive(arr, x, mid + 1, high)
+      }
+    } else {
+      // if low > high => we have searched the whole array without finding the item
+      return -1
+    }
+};
+
+const arr = [1, 5, 6, 7, 8, 9, 10, 12, 15];
+const val = binarySearchRecursive(arr, 15);
+
+console.log('value ', val, arr[val]);
+*/
+
+function binarySearch(arr, x, low = 0, high = arr.length - 1) {
+    const mid = Math.floor((low + high) / 2);
+
+    if (high >= low) {
+        if (arr[mid] === x) {
+            return mid;
+        }
+
+        if (x < arr[mid]) {
+            return binarySearch(arr, x, low, mid - 1);
+        }
+    } else {
+        return -1;
+    }
+}
