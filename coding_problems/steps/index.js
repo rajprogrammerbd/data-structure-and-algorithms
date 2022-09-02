a4678358bcb712371a134543dc136d3e55317899
Raj Dutta <rd2249619@gmail.com>
2022-09-02 16:22:35 +0600

    modify steps

---
+++ coding_problems/steps/index.js
@@ -0,0 +1,21 @@
+function steps(n) {
+    let count = 1;
+    let line = '';
+
+    for (let i = 0; i < n; i++) {
+        for (let z = 1; z <= n; z++) {
+            if (z <= count) {
+                line = "#" + line;
+            } else {
+                line = line + ' ';
+            }
+            if (z === n) {
+                console.log(line);
+            }
+        }
+        line = '';
+        count = count + 1;
+    }
+}
+
+steps(10);
