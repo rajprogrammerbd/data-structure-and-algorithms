527351683a0c901bbee9a74397519722407721e2
Raj Dutta <rd2249619@gmail.com>
2022-10-12 12:39:12 +0600

    add mergeSort

--- coding_problems/binarySearchTree/index.ts
+++ coding_problems/binarySearchTree/index.ts
@@ -113,5 +113,96 @@ tree.insert(5);
 tree.insert(20);
 tree.insert(10);
 
-console.log(tree.find(70));
-console.log(tree);
+class ListNode {
+    val: number
+     next: ListNode | null
+     constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+       this.next = (next===undefined ? null : next)
+    }
+}
+
+ function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): any {
+    let arr1 = [];
+    let arr2 = [];
+    let num1 = 0;
+    let current = l1;
+
+    while (current !== null) {
+        arr1.unshift(current.val);
+        current = current.next;
+    }
+    
+    current = l2;
+    
+    while (current !== null) {
+        arr2.unshift(current.val);
+        current = current.next;
+    }
+    
+    const addition = Number(arr1.join('')) + Number(arr2.join(''));
+    
+    const splitedArr = addition.toString().split('');
+    let res: any;
+
+    for (let i = 0; i < splitedArr.length; i++) {
+        if (res === undefined) {
+            res = new ListNode(Number(splitedArr[i]));
+        } else {
+            console.log('check', res);
+            while (res) {
+                if (res) {
+                    if (!res?.next) {
+                        res.next = new ListNode(Number(splitedArr[i]));
+                        break;
+                    }
+                    res = res.next;
+                }
+                console.log('my res',  res);
+                break;
+            }
+        }
+    }
+    console.log('position', res);
+    return 10;
+};
+
+const param1 = new ListNode(2);
+param1.next = new ListNode(4);
+// param1.next.next = new ListNode(3);
+
+const param2 = new ListNode(5);
+param2.next = new ListNode(6);
+param2.next.next = new ListNode(4);
+
+// const result = addTwoNumbers(param1, param2);
+function recursive(obj: any, val: any): any {
+    if (obj.next !== null) {
+        return recursive(obj.next, val);
+    } else {
+        obj.next = new ListNode(val);
+        return true;
+    }
+}
+
+console.log(recursive(param1, 100), param1);
+
+// console.log('mainly result', result)
+
+
+
+
+            /*
+            while (position) {
+                if (position !== null) {
+                    console.log('check', position);
+                } else {
+                    position = false;
+                }
+                position = position.next;
+        
+                if (position && position.next !== null) {
+                    // position.next = new ListNode(Number(splitedArr[i]));
+                }
+                */
+                // position = position.next;;
